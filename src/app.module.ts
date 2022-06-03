be0f94ff68c42ece0de7dae39482b20947332826
samuel14luis <luismendoza.samuel@gmail.com>
2022-06-03 15:57:34 -0500

    module database working

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,34 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
 import { AppConfigModule } from './app-config/app-config.module';
 import { ConfigCategoryModule } from './config-category/config-category.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [ 
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: [
-        '.env.local',
         '.env', 
         '.env.example'],
     }),
-    TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'quizusr',
-    password: 'nttl@nd315%aLz',
-    database: 'quizappdb',
-    autoLoadEntities: true,
-    synchronize: true,
-    ssl: {
-      ca: process.env.SSL_CERT,
-      rejectUnauthorized: false
-    }
-  }), AppConfigModule, ConfigCategoryModule ],
+    DatabaseModule, AppConfigModule, ConfigCategoryModule, DatabaseModule ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/common/enum/environment.enum.ts
@@ -0,0 +1,4 @@
+export enum Environment {
+    Prod = 'production',
+    Dev = 'development'
+}
