b75991d9d4f213db2e488d18a0390ad40161632f
samuel14luis <luismendoza.samuel@gmail.com>
2022-06-03 10:42:05 -0500

    add swagger and typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [ TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'quizusr',
+    password: 'nttl@nd315%aLz',
+    database: 'quizappdb',
+    autoLoadEntities: true,
+    synchronize: true
+  }) ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,54 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger("Main");
+  //const configService = app.get(ConfigService); TODO: config module
+  const port = 3000; //configService.get("RYS_SYSTEM_PORT")
+  const apiRoot = "api";
+
+  //SETUP
+  app.setGlobalPrefix(apiRoot);
+  app.use(cors());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      forbidUnknownValues: true
+    })
+  )
+  
+  const document = SwaggerModule.createDocument(app, swaggerConfig());  
+  SwaggerModule.setup(`${apiRoot}/docs`, app, document, swaggerOptions());
+
+  logger.log(`Server running on http://localhost:${port}/${apiRoot}`);
+  logger.log(`Swagger UI running on http://localhost:${port}/${apiRoot}/docs`);
+
   await app.listen(3000);
 }
+
+function swaggerConfig() {
+  return new DocumentBuilder()
+    .setTitle('Quiz App')
+    .setDescription('The Quiz App API documentation')
+    .setVersion('1.0')
+    //.addBearerAuth({ type: 'http', scheme: 'bearer', bearerFormat: 'JWT' }, 'jwt-bearer')
+    .build();
+}
+
+function swaggerOptions() {
+  return {
+    explorer: true,
+    swaggerOptions: {
+      filter: true,
+      showRequestDuration: true,
+    }
+  };
+}
+
 bootstrap();
